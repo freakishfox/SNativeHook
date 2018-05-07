d2269854f5838233068a2d449350796272774b61
df <freakishfox@163.com>
2018-05-07 14:40:20 +0800

    'memory

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE    := HookHelper
-LOCAL_SRC_FILES := Hooker.cpp	\
-					Debug.cpp
+LOCAL_MODULE    := Hooker
+LOCAL_SRC_FILES := 	Hooker.cpp	\
+					Debug.cpp	\
+					PosixMemory.cpp
 LOCAL_INCLUDES  := $(LOCAL_PATH)/jni
 LOCAL_CPPFLAGS+= -Wno-error=format-security
 include $(BUILD_STATIC_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,3 @@
-APP_MODULES     := HookHelper
+APP_MODULES     := Hooker
 APP_ABI := armeabi armeabi-v7a
 APP_PLATFORM := android-19
